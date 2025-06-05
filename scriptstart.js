e024e7af18dcbe99fdebb4f9ab265698db610042
annet4588 <annet4588@yahoo.co.uk>
2025-06-05 10:46:48 +0100

    Set the axis.

--- scriptstart.js
+++ scriptstart.js
@@ -3,4 +3,12 @@ const margin = {top: 70, right: 30, bottom:40, left: 80};
 const width = 1200 - margin.left - margin.right;
 const height = 500 - margin.top - margin.bottom;
 
+// Set up x and y scales
+const x = d3.scaleTime()
+   .range([0, width]); // From 0 to the width
+
+const y = d3.scaleLinear()
+   .range([height, 0]);
+
+
 
