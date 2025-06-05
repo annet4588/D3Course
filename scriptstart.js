55dddf6d05021dd0b9dfc76f0ed5e7fc53f248d2
annet4588 <annet4588@yahoo.co.uk>
2025-06-05 10:46:13 +0100

    Set margin convension.

---
+++ scriptstart.js
@@ -0,0 +1,6 @@
+// Set dimentions and margins for the chart
+const margin = {top: 70, right: 30, bottom:40, left: 80};
+const width = 1200 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
+
+
