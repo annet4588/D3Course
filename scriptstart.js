0b733db51744ea89afa187d39a519022ac425fa0
annet4588 <annet4588@yahoo.co.uk>
2025-06-05 11:31:14 +0100

    Load the data from CSV file and process the data.

--- scriptstart.js
+++ scriptstart.js
@@ -18,23 +18,18 @@ const y = d3.scaleLinear()
       .append("g")
         .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// Create a fake dataset
-const dataset = [
-  { date: new Date("2022-01-01"), value: 200 },
-  { date: new Date("2022-02-01"), value: 250 },
-  { date: new Date("2022-03-01"), value: 180 },
-  { date: new Date("2022-04-01"), value: 300 },
-  { date: new Date("2022-05-01"), value: 280 },
-  { date: new Date("2022-06-01"), value: 220 },
-  { date: new Date("2022-07-01"), value: 300 },
-  { date: new Date("2022-08-01"), value: 450 },
-  { date: new Date("2022-09-01"), value: 280 },
-  { date: new Date("2022-10-01"), value: 600 },
-  { date: new Date("2022-11-01"), value: 780 },
-  { date: new Date("2022-12-01"), value: 320 }
-];
+// Load and process the data
+d3.csv("jdi_data_daily.csv").then(function(data){
+    // Parse the date and convert the population to a number
+    const parseDate = d3.timeParse("%Y-%m-%d");
+    data.forEach(d => {
+        d.date = parseDate(d.date);
+        d.population = +d.population;
+    });
+    
+    console.log(data);
+});
 
-console.log(dataset);
 
 //Define x and y domains
 x.domain(d3.extent(dataset, d => d.date)); // Include all the dates from the dataset
