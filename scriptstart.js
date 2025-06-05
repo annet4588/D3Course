126a4fecbea9cce2176f0dd50884fa7b4b357608
annet4588 <annet4588@yahoo.co.uk>
2025-06-05 10:53:11 +0100

    Create fake data

--- scriptstart.js
+++ scriptstart.js
@@ -18,5 +18,20 @@ const y = d3.scaleLinear()
       .append("g")
         .attr("tranform", `translate(${margin.left}, ${margin.top})`);
 
+// Create a fake dataset
+const dataset = [
+  { date: new Date("2022-01-01"), value: 200 },
+  { date: new Date("2022-02-01"), value: 250 },
+  { date: new Date("2022-03-01"), value: 180 },
+  { date: new Date("2022-04-01"), value: 300 },
+  { date: new Date("2022-05-01"), value: 280 },
+  { date: new Date("2022-06-01"), value: 220 },
+  { date: new Date("2022-07-01"), value: 300 },
+  { date: new Date("2022-08-01"), value: 450 },
+  { date: new Date("2022-09-01"), value: 280 },
+  { date: new Date("2022-10-01"), value: 600 },
+  { date: new Date("2022-11-01"), value: 780 },
+  { date: new Date("2022-12-01"), value: 320 }
+];
 
-
+console.log(dataset);
